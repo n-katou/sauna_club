a646d35ba8ada02418d09028f99f4173a99e6aad
n-katou <naoto.light@gmail.com>
2022-07-10 14:48:19 +0000

    コメント機能の途中

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,12 +3,23 @@ class CommentsController < ApplicationController
   end
 
   def create
+    post = Post.find(params[:post_id])
+    comment = current_customer.posts.new(comment_params)
+    comment.post_id = post.id
+    comment.save
+    redirect_to post_path(post.id)
   end
 
   def update
   end
 
   def destroy
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:comment_content)
   end
 
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -15,16 +15,25 @@ class PostsController < ApplicationController
   end
 
   def edit
+    @post = Post.find(params[:id])
   end
 
   def update
+    post = Post.find(params[:id])
+    post.update(post_params)
+    redirect_to post_path(post.id)
   end
 
   def destroy
+    post = Post.find(params[:id])
+    post.destroy
+    redirect_to posts_path
   end
 
   def show
     @post = Post.find(params[:id])
+    @comment = Comment.new
+    @comments = Comment.all
   end
 
   private
