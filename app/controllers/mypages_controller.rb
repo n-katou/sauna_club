56a1855e854e493937ff7488beaca5e5a779d634
n-katou <naoto.light@gmail.com>
2022-07-21 07:26:48 +0000

    コメント編集レイアウト完成

--- app/controllers/mypages_controller.rb
+++ app/controllers/mypages_controller.rb
@@ -1,6 +1,6 @@
 class MypagesController < ApplicationController
   def show
     @customer = Customer.find(current_customer.id)
-    @posts = @customer.posts.page(params[:page])
+    @posts = @customer.posts.order("created_at DESC").page(params[:page])
   end
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.page(params[:page])
+    @posts = Post.order("created_at DESC").page(params[:page]).per(5)
   end
 
   def new
