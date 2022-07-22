6e1bf838a0f911ae9f6dbecf65ae75d15da03211
n-katou <naoto.light@gmail.com>
2022-07-22 10:12:29 +0000

    レイアウトアップデート

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.order("created_at DESC").page(params[:page]).per(8)
+    @posts = Post.order("created_at DESC").page(params[:page]).per(5)
   end
 
   def new
