6c337c939a067b9b676fd53e08945e957e0d6633
n-katou <naoto.light@gmail.com>
2022-07-24 02:38:46 +0000

    update

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,6 +1,7 @@
 class HomesController < ApplicationController
   def top
-    @posts = Post.all.order("RAND()").limit(5)
+    rand = Rails.env.production? ? "RANDOM()" : "rand()"
+    @posts = Post.order(rand).limit(5)
   end
 
   def about
