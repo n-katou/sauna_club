acf85375384d713c4b7363ca0ce38fc17d53634d
n-katou <naoto.light@gmail.com>
2022-07-24 09:07:10 +0000

    update

--- app/models/post.rb
+++ app/models/post.rb
@@ -18,7 +18,7 @@ class Post < ApplicationRecord
   def get_post_image(width, height)
     unless post_image.attached?
       file_path = Rails.root.join('app/assets/images/no_image.jpg')
-      post_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+      post_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpg')
     end
     post_image.variant(resize_to_limit: [width, height]).processed
   end
