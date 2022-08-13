f535d9ce0c4a2c659efbe203a6edf4546ef0280a
n-katou <102382177+n-katou@users.noreply.github.com>
2022-08-13 10:52:34 +0000

    チャット機能、コメント非同期通信実装

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -32,12 +32,11 @@ class CommentsController < ApplicationController
   def destroy
     @comment = Comment.find(params[:id])
     if @comment.destroy
-      # @comment = Comment.new
-      # @post = Post.find(params[:post_id])
-      # @comment.post_id = @post.id
-      # @comments = @post.comments.order("created_at DESC").page(params[:page]).per(5)
-      # render :delete
-      redirect_to post_path(@comment.post.id) #coment.post_idか？
+      @comment = Comment.new
+      @post = Post.find(params[:post_id])
+      @comment.post_id = @post.id
+      @comments = @post.comments.order("created_at DESC").page(params[:page]).per(5)
+      render :delete #jsを読み込んでいる
     end
   end
 
--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -9,6 +9,7 @@ class RoomsController < ApplicationController
   end
 
   def create
+    @customer = Customer.find(params[:customer_id])
     customer_rooms = CustomerRoom.find_customer_rooms(current_customer, @customer)
     if customer_rooms.nil?
       room = Room.create
@@ -25,7 +26,7 @@ class RoomsController < ApplicationController
     room.check_chats_notification(current_customer)
     customer_id = room.customer_rooms.where.not(customer_id: current_customer.id).select(:customer_id)
     @customer = Customer.where(id: customer_id).first
-    @chats = room.chats.includes(:customer)
+    @chats = room.chats.includes(:customer).page(params[:page]).per(5)
     @chat = Chat.new(room_id: room.id)
   end
 
@@ -36,7 +37,7 @@ class RoomsController < ApplicationController
     flash[:danger] = "ユーザーにはアクセスする権限がありません"
     redirect_to root_path
   end
-  
+
   # def show
   #   @customer = Customer.find(params[:id])
   #   rooms = current_customer.customer_rooms.pluck(:room_id)
